b0ce27135b4da3edf272bcbaba1d5fa3ae94b197
Alisa Ts <freiheiit92@mail.ru>
2022-01-27 14:41:16 +0300

    Initial Commit

---
+++ StateAndDataFlow/RegisterView.swift
@@ -0,0 +1,48 @@
+//
+//  RegisterView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 26.01.2022.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @EnvironmentObject private var userManager: UserManager
+    
+    @State private var name = ""
+    @State private var isValid = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                TextField("Enter your name", text: $userManager.user.name)
+                    .multilineTextAlignment(.center)
+                Text("\(userManager.user.name.count)")
+                    .foregroundColor(userManager.isValid ? .green : .red)
+                    .padding()
+            }
+            Button(action: registerUser) {
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                    Text("OK")
+                }
+            }
+            .disabled(!self.userManager.isValid)
+        }
+    }
+    
+    private func registerUser() {
+        if !userManager.user.name.isEmpty {
+            
+            userManager.isRegister.toggle()
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+        
+    }
+}
---
+++ StateAndDataFlow/UserManager.swift
@@ -0,0 +1,28 @@
+//
+//  UserManager.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 26.01.2022.
+//
+
+import Combine
+import Foundation
+
+class UserManager: ObservableObject {
+    @Published var isRegister = false
+    @Published var user = User()
+    var isValid: Bool {
+        user.name.count >= 3
+    }
+    
+    private var cancellableSet: Set<AnyCancellable> = []
+    
+    init() {}
+    init(user: User) {
+        self.user = user
+    }
+    
+}
+struct User {
+    var name = ""
+}
